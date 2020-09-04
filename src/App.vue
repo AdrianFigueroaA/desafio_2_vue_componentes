6da1c38bea143fa6bc75db9b50b16deeffa79894
Adrian <revanskyw@gmail.com>
2020-09-04 17:05:54 -0400

    primera version

---
+++ src/App.vue
@@ -0,0 +1,63 @@
+<template>
+
+  <div id="app" > 
+    <input v-model="tarea" type="text" autofocus @keyup.enter="agregar"/>
+    <button @click="agregar">Agregar tarea</button>
+    <Tarea
+      v-for="(tarea, i) in tareas "
+      :tarea="tarea"
+      :indice="i"
+      :key="i"
+      @eliminar="eliminar"
+    ></Tarea>
+  </div>
+
+</template>
+
+
+<script>
+import Tarea from "./components/Tarea";
+export default {
+  name: "cosasPorhacer",
+  data() {
+    return {
+      tarea: "",
+      tareas: [],
+    };
+  },
+  components: {
+    Tarea,
+  },
+  methods: {
+    agregar() {
+      this.tareas.push(this.tarea);
+      this.tarea = "";
+    },
+    eliminar(indice) {
+      this.tareas.splice(indice, 1);
+    },
+  },
+};
+</script>
+
+
+
+<style lang="scss">
+
+body{
+
+  background: green;
+}
+
+h1{
+
+text-align: center;
+
+}
+#app {
+  text-align: center;
+}
+</style>
+
+
+
---
+++ src/components/Tarea.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    {{tarea}}
+    <button @click="vaciar">Eliminar tarea</button>
+  </div>
+</template>
+
+<script>
+export default {
+name: "Tarea",
+  props: {
+    tarea: String,
+    indice: Number,
+  },
+  methods: {
+    vaciar() {
+      this.$emit("eliminar", this.indice);
+    },
+  },
+};
+</script>
